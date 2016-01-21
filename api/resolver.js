988a9f9f6a46bbdfaffe0ae23a14e77314f4a940
SamEBae <baesammie@gmail.com>
2016-01-21 12:23:30 -0500

    bar chart support and image import

--- api/resolver.js
+++ api/resolver.js
@@ -5,9 +5,7 @@ module.exports = function(req, res) {
     return;
   }
 
-  var width = data.width > 600 ? 600 : data.width;
   var html  = data.html;
-      html += '<style>.legend rect {fill:white;opacity:0.4;}</style>';
   res.json({
     body: html
   });
